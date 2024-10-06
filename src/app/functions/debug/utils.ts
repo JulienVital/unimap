d8a03a1145ffb27f011ea68cce36e272f542c385
Julien Vital <julien.vital@yahoo.fr>
2024-10-06 21:16:31 +0200

    more classroom

--- src/app/functions/debug/utils.ts
+++ src/app/functions/debug/utils.ts
@@ -1,8 +1,16 @@
-export const handleCanvasClick = (event: React.MouseEvent<HTMLCanvasElement>, canvasRef: React.MutableRefObject<HTMLCanvasElement |null>) => {
-    const rect = canvasRef.current?.getBoundingClientRect();
-    if (rect) {
-      const x = ((event.clientX - rect.left) / rect.width) * 100;
-      const y = ((event.clientY - rect.top) / rect.height) * 100;
-      console.log(`Coordonnées: left: ${x.toFixed(2)}%, top: ${y.toFixed(2)}%`);
-    }
-  };
+export const handleCanvasClick = (
+  event: React.MouseEvent<HTMLCanvasElement>,
+  canvasRef: React.MutableRefObject<HTMLCanvasElement | null>
+) => {
+  const rect = canvasRef.current?.getBoundingClientRect();
+  if (rect) {
+    const x = ((event.clientX - rect.left) / rect.width) * 100;
+    const y = ((event.clientY - rect.top) / rect.height) * 100;
+    console.log(`
+          "position": {
+          "top": ${y.toFixed(2)},
+          "left": ${x.toFixed(2)}
+        },
+        `);
+  }
+};
