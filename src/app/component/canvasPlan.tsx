2d1028aa88ea39b6ba78a8226aaed713f7e01c87
Julien Vital <julien.vital@yahoo.fr>
2024-10-07 21:42:04 +0200

    remove use effect

--- src/app/component/canvasPlan.tsx
+++ src/app/component/canvasPlan.tsx
@@ -9,26 +9,6 @@ interface CanvasPlanProps
 
 const CanvasPlan = forwardRef<HTMLCanvasElement, CanvasPlanProps>(
   ({ onClick, ...rest }, ref) => {
-    useEffect(() => {
-      const canvas = ref as React.RefObject<HTMLCanvasElement>;
-      if (canvas.current) {
-        const updateCanvasSize = () => {
-          if(  canvas.current){
-            const footerHeight = 64; // Remplacez ceci par la hauteur de votre footer
-            canvas.current.height = window.innerHeight - footerHeight; // Ajustez la hauteur
-            canvas.current.width = window.innerWidth; // Largeur totale de la fenêtre
-          }
-        };
-
-        updateCanvasSize(); // Ajustez la taille initiale
-
-        window.addEventListener("resize", updateCanvasSize); // Écoutez les redimensionnements
-
-        return () => {
-          window.removeEventListener("resize", updateCanvasSize); // Nettoyez l'écouteur
-        };
-      }
-    }, [ref]);
 
     return (
       <canvas
